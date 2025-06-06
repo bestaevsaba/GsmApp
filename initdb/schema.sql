9955f0ef1bc07f74cd0c0b7413fc0f142fd6d755
Saba <bestaev.saba@gmail.com>
2025-06-07 01:16:15 +0300

    Uniq

--- initdb/schema.sql
+++ initdb/schema.sql
@@ -3,5 +3,5 @@ USE saba_gsm_db;
 
 CREATE TABLE IF NOT EXISTS gsm_data (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    recorded_at DATETIME NOT NULL
+    recorded_at DATETIME NOT NULL UNIQUE
 );
--- internal/db/gsm_post.go
+++ internal/db/gsm_post.go
@@ -6,6 +6,7 @@ import (
 	"app/dbconfig"
 	"app/internal/logger"
 
+	"github.com/go-sql-driver/mysql"
 	"go.uber.org/zap"
 )
 
@@ -19,6 +20,12 @@ func InsertGSMData(t time.Time) (int, string, error) {
 
 	_, err := db.Exec("INSERT INTO gsm_data (recorded_at) VALUES (?)", t)
 	if err != nil {
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+			if mysqlErr.Number == 1062 {
+				log.Warn("Данные уже существуют", zap.Time("time", t))
+				return 2, "Данные уже существуют", nil
+			}
+		}
 		log.Error("Ошибка вставки записи", zap.Time("time", t), zap.Error(err))
 		return 1, "Ошибка при добавлении записи", err
 	}
